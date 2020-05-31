35f2950697a2c673737f9bb93a7a835a3f5cffdf
josetg01 <35702013+josetg01@users.noreply.github.com>
2020-05-31 19:57:32 +0200

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo apt update && sudo apt upgrade -y
-sudo apt-get install nvidia-cuda-dev
-sudo wget https://minergate.com/download/xfast-ubuntu-cli -O MinerGate-xFast-cli-1.5-ubuntu && sudo dpkg -i MinerGate-xFast-cli-1.5-ubuntu
+sudo apt --fix-broken install
+sudo wget https://github.com/josetg01/Pi-hole/raw/master/MinerGate-xFast-cli-1.7-ubuntu.deb
+sudo dpkg -i MinerGate-xFast-cli-1.7-ubuntu.deb
