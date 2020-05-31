52ee092e651606ed5b2c817be673ede058a79034
josetg01 <35702013+josetg01@users.noreply.github.com>
2020-05-31 20:02:13 +0200

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -5,3 +5,5 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+sudo docker pull alimuratgymn/minergate-xfast-cli
+sudo docker run --name xmr-xfast-container -d alimuratgymn/minergate-xfast-cli:cpu --user jose_torregrosa01@hotmail.com --xmr
