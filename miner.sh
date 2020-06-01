788b51878d76197b12d927c53bb97aad7a550c6b
josetg01 <35702013+josetg01@users.noreply.github.com>
2020-06-01 15:05:29 +0200

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+sudo apt update
 sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
 git clone https://github.com/xmrig/xmrig.git
 cd xmrig && mkdir build && cd build
-cmake ..
-make
+sudo cmake ..
+sudo make
 ./xmrig -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45700 -u jose_torregrosa01@hotmail.com -p x
