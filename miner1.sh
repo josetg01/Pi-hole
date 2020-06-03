b010a46bd9ca8c92754d956d942a7229533ec33f
josetg01 <35702013+josetg01@users.noreply.github.com>
2020-06-03 17:20:16 +0200

    Create miner1.sh

---
+++ miner1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt update
+sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
+git clone https://github.com/xmrig/xmrig.git
+cd xmrig && mkdir build && cd build
+sudo cmake ..
+sudo make
+./xmrig --donate-level 1 -o pool.supportxmr.com:443 -u 4BrL51JCc9NGQ71kWhnYoDRffsDZy7m1HUU7MRU4nUMXAHNFBEJhkTZV9HdaL4gfuNBxLPc3BeMkLGaPbF5vWtANQsGNJWFrDRqK7aZBGL -k --tls
