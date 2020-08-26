1b1146da6b393646d84460f6c14ccf9b181d9048
josetg01 <35702013+josetg01@users.noreply.github.com>
2020-08-26 13:23:58 +0200

    Update miner1.sh

--- miner1.sh
+++ miner1.sh
@@ -5,4 +5,4 @@ git clone https://github.com/xmrig/xmrig.git
 cd xmrig && mkdir build && cd build
 sudo cmake ..
 sudo make
-./xmrig --donate-level 1 -o pool.supportxmr.com:443 -u 4BrL51JCc9NGQ71kWhnYoDRffsDZy7m1HUU7MRU4nUMXAHNFBEJhkTZV9HdaL4gfuNBxLPc3BeMkLGaPbF5vWtANQsGNJWFrDRqK7aZBGL -k --tls
+./xmrig --donate-level 1 -o pool.supportxmr.com:443 -u 4BrL51JCc9NGQ71kWhnYoDRffsDZy7m1HUU7MRU4nUMXAHNFBEJhkTZV9HdaL4gfuNBxLPc3BeMkLGaPbF5vWtANQtawHj5ApfiHgEX2hr -k --tls
